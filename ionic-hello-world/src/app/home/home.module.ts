437ec9dc87e5bcdcbce2c90b9216c4dcbcfcb96e
Maksym Rybak <rybak.maksym@gmail.com>
2020-08-18 18:21:38 +0200

    example of how to use alerts and modals in ionic

--- ionic-hello-world/src/app/home/home.module.ts
+++ ionic-hello-world/src/app/home/home.module.ts
@@ -3,8 +3,9 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
-
 import { HomePageRoutingModule } from './home-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalComponentModule } from '../modal/modal.module';
 
 
 @NgModule({
@@ -12,7 +13,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    HttpClientModule,
+    ModalComponentModule
   ],
   declarations: [HomePage]
 })
--- ionic-hello-world/src/app/home/home.page.ts
+++ ionic-hello-world/src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertController, ModalController } from '@ionic/angular';
+import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,55 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  items = [];
 
-  constructor() {}
+  constructor(private http: HttpClient, private alertCtrl: AlertController, private modalCtrl: ModalController) {
 
+  }
+
+  // ngOnInit() {
+  //   console.log('ngOnInit...');
+  // }
+
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter...');
+
+    this.http.get('https://randomuser.me/api/').subscribe( (res: any) => this.items = res.results);
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave...');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave...');
+  }
+
+  async showAlert() {
+    const alert = await this.alertCtrl.create({
+      message: 'Show an Alert',
+      subHeader: 'My subHeader',
+      header: 'My header',
+      buttons: [
+        {
+          role: 'Cancel',
+          text: 'Close',
+          handler: () => console.log('close btn clicked')
+        }
+      ]
+    });
+
+    alert.present();
+  }
+
+  async showModal(item) {
+    const modal = await this.modalCtrl.create({
+      component: ModalComponent,
+      componentProps: {
+        item
+      }
+    });
+
+    modal.present();
+  }
 }
