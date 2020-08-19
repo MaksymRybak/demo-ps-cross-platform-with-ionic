f50e6b49d8b702e6807cdf180a1cac35449430b9
Maksym Rybak <rybak.maksym@gmail.com>
2020-08-19 15:18:58 +0200

    add tabs navigation

--- ionic-hello-world/src/app/app-routing.module.ts
+++ ionic-hello-world/src/app/app-routing.module.ts
@@ -3,18 +3,26 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tabs',
     pathMatch: 'full'
   },
   {
-    path: 'detail/:id',
-    loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then( m => m.TabsPageModule)
   },
+  // {
+  //   path: 'home',
+  //   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  // },
+  // {
+  //   path: 'detail/:id',
+  //   loadChildren: () => import('./detail/detail.module').then( m => m.DetailPageModule)
+  // },
+  // {
+  //   path: 'tab2',
+  //   loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  // },
 ];
 
 @NgModule({
