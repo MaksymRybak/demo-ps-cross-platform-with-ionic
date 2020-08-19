625712c78044f73fe96f5326610edfbf28f070d8
Maksym Rybak <rybak.maksym@gmail.com>
2020-08-19 16:43:42 +0200

    add service worker support with a small example

--- ionic-hello-world/src/app/app.component.ts
+++ ionic-hello-world/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController,  } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -13,9 +15,34 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private swUpdate: SwUpdate,
+    private toastCtrl: ToastController
   ) {
     this.initializeApp();
+  }
+
+  async ngOnInit() {
+    this.swUpdate.available.subscribe(async results => {
+      // create toast controller
+      const toast = await this.toastCtrl.create({
+        message: 'Update Available',
+        position: 'bottom',
+        buttons: [
+          {
+            text: 'Reload',
+            role: 'cancel'
+          }
+        ]
+      });
+
+      await toast.present();
+
+      // show info to end user when service worker has updates
+      toast.onDidDismiss()
+        .then(() => this.swUpdate.activateUpdate())
+        .then(() => window.location.reload());
+    });
   }
 
   initializeApp() {
